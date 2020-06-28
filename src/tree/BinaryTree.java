1f9415d1fc6a3b746acbbbce5c9fcf2a9285ab27
Collins Liu <CollinsTroubadour@gmail.com>
2020-06-28 00:58:53 -0400

    add iteration add method and height counting method, traverse with inorder traversal method

--- src/tree/BinaryTree.java
+++ src/tree/BinaryTree.java
@@ -10,6 +10,11 @@ public class BinaryTree {
 		Node leftChild = null;
 		Node rightChild = null;
 		Node parent = null;
+		int leftChildHeight = 0;
+		int rightChildHeight = 0;
+		// if negative, left tree is taller, 
+		// the other way round.
+		int heightDiff;
 		Element val;
 
 		public Node(Node leftChild, Node rightChild, Node parent, Element val) {
@@ -34,7 +39,7 @@ public class BinaryTree {
 	
 	
 	// set up basic properties of this tree
-	private Node root;
+	private Node root = null;
 	private int size = 0;
 	
 	//constructor of the binary tree
@@ -79,8 +84,7 @@ public class BinaryTree {
 		}
 		
 		if(node.val.compareTo(e)==0){
-			flag = true;
-			addElementWithParent(node,node.leftChild, e);
+			System.out.println("discard " + node.toString());
 		}else if(node.val.compareTo(e)>0){
 			flag = true;
 			addElementWithParent(node,node.leftChild, e);
@@ -95,17 +99,75 @@ public class BinaryTree {
 		if(null!=node){
 			traverseTree(node.leftChild);
 			System.out.print(node.val.toString()+" -> ");
+			System.out.println(node.heightDiff + " " + node.leftChildHeight + " " + node.rightChildHeight);
 			traverseTree(node.rightChild);
 		}
 	}
 	
+	public int getBalance(Node node){
+		if(null==node.leftChild && null==node.rightChild){
+			node.heightDiff = 0;
+			return 0;
+		}
+		if(null == node.rightChild){
+			node.leftChildHeight = getBalance(node.leftChild)+1;
+			node.rightChildHeight = 0;
+		}else if(null == node.leftChild){
+			node.rightChildHeight = getBalance(node.rightChild)+1;
+			node.leftChildHeight = 0;
+		}else{
+			node.rightChildHeight = getBalance(node.rightChild)+1;
+			node.leftChildHeight = getBalance(node.leftChild)+1;
+		}
+		node.heightDiff = node.leftChildHeight - node.rightChildHeight;
+		return (node.leftChildHeight>=node.rightChildHeight)?node.leftChildHeight:node.rightChildHeight;
+	}
+	
+	// left is true, else the other way round
+	private boolean itr_flag = true;
+	// add new elements, iteration
+	public boolean addElement(Node node, Element e){
+		if(null==node){
+			root = new Node(e);
+			size ++;
+			return true;
+		}
+		// temp points to the position where
+		// new node is to be added
+		Node temp = node;
+		// pointer will always point to the 
+		// last leaf node in the tree
+		Node pointer = node;
+		while(null!=temp){
+			pointer = temp;
+			if(node.val.compareTo(e)==0){
+				System.out.println("discard:" + node.toString());
+				return false;
+			}else if(temp.val.compareTo(e)>0){
+				temp = pointer.leftChild;
+				itr_flag = true;
+			}else if(temp.val.compareTo(e)<0){
+				temp = pointer.rightChild;
+				itr_flag = false;
+			}
+		}
+		temp = new Node(e);
+		if(itr_flag){
+			pointer.leftChild = temp;
+		}else{
+			pointer.rightChild = temp;
+		}
+		temp.parent = pointer;
+		size ++;
+		return true;
+	}
 	
 	public static void main(String[] args) {
 		BinaryTree bst = new BinaryTree();
 
 		List<Element> list = new LinkedList<Element>();
-		list.add(new Element(1,"abcd"));
 		list.add(new Element(16,"abcd"));
+		list.add(new Element(11,"abcd"));
 		list.add(new Element(23,"abcd"));
 		list.add(new Element(5,"abcd"));
 		list.add(new Element(8,"abcd"));
@@ -117,9 +179,11 @@ public class BinaryTree {
 		list.add(new Element(6,"abcd"));
 		list.add(new Element(9,"abcd"));
 		for(Element items: list){
-			bst.addElement(items);
+			//bst.addElement(items);
+			bst.addElement(bst.root,items);
 		}
 		
+		int w = bst.getBalance(bst.root);
 		bst.traverseTree(bst.root);
 	}
 	
